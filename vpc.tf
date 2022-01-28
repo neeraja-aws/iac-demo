1bb8dbe1009ed3648d177e06b2a21ec59cee15b3
neeraja-aws <85746777+neeraja-aws@users.noreply.github.com>
2022-01-28 22:47:59 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-
+  region = "us-east-2"
   tags = {
     Name = "demo-vpc"
     Purpose = "Jenkins Demo"
